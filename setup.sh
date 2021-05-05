42057d2fd1762efd1cff2b300574e0dc3e59d025
Aaron <56976471+aaronwangj@users.noreply.github.com>
2021-05-04 21:37:04 -0400

    modified setup

--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@ echo "\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-\n\
+[theme]\n\
+primaryColor = '#E694FF'\n\
+backgroundColor = '#00172B'\n\
+secondaryBackgroundColor = '#0083B8'\n\
+textColor = '#C6CDD4'\n\
+font = 'sans serif'\n\
 " > ~/.streamlit/config.toml
